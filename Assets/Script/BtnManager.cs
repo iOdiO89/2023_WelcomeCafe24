1898e5f6af28f53837ff3616398564c980c7d5a4
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2023-01-23 03:18:58 +0900

    230123/정선아/#301, #303
    #301 - 드래그로 레시피 확인 가능
    #303 - 주문 타이머 설정, 재화/명성 조절 가능

--- Assets/Script/BtnManager.cs
+++ Assets/Script/BtnManager.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BtnManager : MonoBehaviour
-{
+{   
+    public List<GameObject> recipeList = new List<GameObject>();
+    public List<GameObject> menuList = new List<GameObject>();
+
+    List<bool> activeMenuList = new List<bool>();
 
     public void StartNewBtn(){    // StartScene-새로하기
         SceneManager.LoadScene("PrologScene");
@@ -18,5 +24,30 @@ public class BtnManager : MonoBehaviour
         SceneManager.LoadScene("MainScene");
     }
 
+    public void ShowRecipe(int index){
+        int recipeSize = recipeList.Count;
+        for(int i=0; i<recipeSize; i++){
+            if(i==index){
+                recipeList[i].SetActive(true);
+            }
+            else{
+                recipeList[i].SetActive(false);
+            }
+        }
+    }
 
+    public void MenuBtn(){
+        /*activeMenuList를 활용하여 해당 메뉴가 해금되지 않은 경우
+        NotActiveRecipe가 출력되지 않게 해야 함(아직 구현X)*/
+
+        GameObject clickObject = EventSystem.current.currentSelectedGameObject;
+        string name = clickObject.name;
+        string stringRes = name.Substring(name.Length-2);
+        int intRes=-1;
+        if(!int.TryParse(stringRes, out intRes)){ // 두자리수일때
+            stringRes = name.Substring(name.Length-1);
+        }
+        // print(System.Convert.ToInt32(stringRes) + "번 메뉴");
+        ShowRecipe(System.Convert.ToInt32(stringRes)-1);
+    }
 }
